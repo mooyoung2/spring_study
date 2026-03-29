a6f5b4020065290c1243712691592aca01180316
mooyoung2 <kkmy0822@naver.com>
2026-03-29 18:28:00 +0900

    feat : 회원 리포지토리 테스트 케이스 작성

--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -22,14 +22,19 @@ public class MemoryMemberRepository implements MemberRepository{
     }
 
     @Override
-    public Optional<Member> findbyName(String name) {
+    public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
 
+
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public  void clearStore() {
+        store.clear();
     }
 }
